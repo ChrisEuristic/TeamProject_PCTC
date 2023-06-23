6e31e7f9b9f7a2b2893268461514100b9afb4be0
ChrisEuristic <eanair@kakao.com>
2023-06-23 19:00:39 +0900

    입출항예정표 페이지 구현

--- pctc-front-next/src/app/components/client/calendar/DetailView.tsx
+++ pctc-front-next/src/app/components/client/calendar/DetailView.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Dispatch, SetStateAction } from "react";
 import { GrFormClose } from "react-icons/gr";
+import { ShipData } from "./ShipDataType";
 
 export default function DetailView({
   x,
@@ -10,7 +11,7 @@ export default function DetailView({
 }: {
   x: number;
   y: number;
-  info: string;
+  info: ShipData;
   setDetailViewState: Dispatch<SetStateAction<JSX.Element | undefined>>;
 }) {
   return (
@@ -23,7 +24,13 @@ export default function DetailView({
         >
           <GrFormClose />
         </button>
-        <h1>{info}</h1>
+        <h1>{info.title}</h1>
+        <div><span className="ship-detail-span-left">모선</span><span className="ship-detail-span-right">{info.shipName}</span></div>
+        <div><span className="ship-detail-span-left">선사</span><span className="ship-detail-span-right">{info.shipOwner}</span></div>
+        <div><span className="ship-detail-span-left">적하</span><span className="ship-detail-span-right">{info.discharge}</span></div>
+        <div><span className="ship-detail-span-left">양하</span><span className="ship-detail-span-right">{info.loading}</span></div>
+        <div><span className="ship-detail-span-left">입항</span><span className="ship-detail-span-right">{info.scheduledArrivalTime}</span></div>
+        <div><span className="ship-detail-span-left">출항</span><span className="ship-detail-span-right">{info.departureTime}</span></div>
       </main>
       <style jsx>{`
         main {
@@ -31,8 +38,8 @@ export default function DetailView({
           top: ${y - 200}px;
           left: ${x - 250}px;
           z-index: 100;
-          width: 200px;
-          height: 300px;
+          width: 300px;
+          height: 290px;
           background-color: white;
           border: solid 1px #14141480;
           border-radius: 5px;
@@ -44,11 +51,30 @@ export default function DetailView({
         h1 {
           font-size: 1.5rem;
           font-weight: bold;
+          margin: 1rem 0;
         }
         button {
           position: absolute;
           top: 5px;
           right: 5px;
+        }
+        div {
+          width: 100%;
+          display: flex;
+          justify-content: space-evenly;
+          margin: 0.25rem 0.5rem;
+        }
+        .ship-detail-span-left {
+          width: 50px;
+          display: flex;
+          justify-content: center;
+          font-weight: bold;
+        }
+        .ship-detail-span-right {
+          width: 150px;
+          display: flex;
+          justify-content: center;
+          border-bottom: 1px solid #141414;
         }
       `}</style>
     </>
